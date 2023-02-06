5b334b78ad13ed70da7c89b666ff546dcb0dc06a
Naveen Kunapaneni <nv676973@dal.ca>
2023-02-05 22:19:23 -0400

    Initial spring project

---
+++ src/main/java/com/project/group17/FindMyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.project.group17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyHomeApplication.class, args);
+	}
+
+}
