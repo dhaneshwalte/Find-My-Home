51e5cf7bf837c2316d2b8b349a78b3dcfda64df2
Muskan Vazirani <muskan@dal.ca>
2023-03-12 20:41:06 -0300

    Resolved build issue

--- Frontend/react-group17/src/Pages/Match/Match.js
+++ Frontend/react-group17/src/Pages/Match/Match.js
@@ -8,90 +8,6 @@ const Match = () => {
 
     const [isLoading, setLoading] = useState(true);
     const [users, setUsers] = useState([]);
-    // const [users, setUsers] = useState([{
-    //     "gender": "female",
-    //     "name": {
-    //         "title": "Monsieur",
-    //         "first": "Yvan",
-    //         "last": "Olivier"
-    //     },
-    //     "location": {
-    //         "city": "Dartmouth",
-    //         "state": "NS",
-    //         "country": "Switzerland",
-    //         "postcode": 4678
-    //     },
-    //     "picture": {
-    //         "thumbnail": "https://randomuser.me/api/portraits/thumb/women/65.jpg"
-    //     },
-    //     "drinker": "Yes",
-    //     "smoker": "No",
-    //     "meal": "Vegan",
-    //     "id": 1
-    //     },
-    //     {
-    //     "gender": "female",
-    //     "name": {
-    //         "title": "John",
-    //         "first": "Doe",
-    //         "last": "Olivier"
-    //     },
-    //     "location": {
-    //         "city": "Lower Sackville",
-    //         "state": "NS",
-    //         "country": "Switzerland",
-    //         "postcode": 4678
-    //     },
-    //     "picture": {
-    //         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/2.jpg"
-    //     },
-    //     "drinker": "Yes",
-    //     "smoker": "No",
-    //     "meal": "Non Vegetarian",
-    //     "id": 2
-    //     },
-    //     {
-    //     "gender": "female",
-    //     "name": {
-    //         "title": "Donna",
-    //         "first": "Put",
-    //         "last": "Olivier"
-    //     },
-    //     "location": {
-    //         "city": "Halifax",
-    //         "state": "NS",
-    //         "country": "Switzerland",
-    //         "postcode": 4678
-    //     },
-    //     "picture": {
-    //         "thumbnail": "https://randomuser.me/api/portraits/thumb/women/12.jpg"
-    //     },
-    //     "drinker": "Yes",
-    //     "smoker": "No",
-    //     "meal": "Vegan",
-    //     "id": 3
-    //     },
-    //     {
-    //     "gender": "female",
-    //     "name": {
-    //         "title": "George",
-    //         "first": "George",
-    //         "last": "Buck"
-    //     },
-    //     "location": {
-    //         "city": "Bedford",
-    //         "state": "NS",
-    //         "country": "Switzerland",
-    //         "postcode": 4678
-    //     },
-    //     "picture": {
-    //         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg"
-    //     },
-    //     "drinker": "Yes",
-    //     "smoker": "No",
-    //     "meal": "Vegan",
-    //     "id": 4
-    //     }]);
     
     useEffect(() => {
         getRoommateList()
@@ -128,7 +44,6 @@ const Match = () => {
             </div>
         )
     }
-    const empty = () => (<div></div>)
 
     return (
         <div className="">
