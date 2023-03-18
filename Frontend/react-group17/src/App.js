952c64ec6baad2fd29483f23a21dc035bc24ec5c
kunapaneni <nv676973@dal.ca>
2023-03-18 02:42:15 -0300

    Removed merge conflicts

--- Frontend/react-group17/src/App.js
+++ Frontend/react-group17/src/App.js
@@ -4,7 +4,6 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import UserPreference from './Pages/UserPreferences';
-import Match from './Pages/Match/Match';
 import ProfilePage from './Pages/Profile/profile';
 import Navbar from './Pages/Navbar/navbar'
 
@@ -18,7 +17,6 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" exact element={<Register/>}/>
         <Route path="/user-preference" element={<UserPreference/>}/>
-        <Route path="/match" element={<Match/>}/>
         <Route path="/profilepage" element={<ProfilePage/>}/>
         </Routes>
       </BrowserRouter>
