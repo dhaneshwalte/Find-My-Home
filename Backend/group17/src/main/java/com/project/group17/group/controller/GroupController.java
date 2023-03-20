0abd5924f40db816743f09af281cc841aad0558d
Muskan Vazirani <muskan@dal.ca>
2023-03-20 17:07:04 -0300

    Added code to fetch my group members

--- Backend/group17/src/main/java/com/project/group17/group/controller/GroupController.java
+++ Backend/group17/src/main/java/com/project/group17/group/controller/GroupController.java
@@ -1,7 +1,41 @@
 package com.project.group17.group.controller;
 
+import com.project.group17.group.repository.GroupRepository;
+import com.project.group17.match.service.MatchService;
+import com.project.group17.user.entity.User;
+import com.project.group17.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api/v1")
 public class GroupController {
+
+    @Autowired
+    GroupRepository groupRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    MatchService matchService;
+    @GetMapping("/my-group")
+    public ResponseEntity<List<Map<String, String>>> getGroupMembers(){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Integer> groupMembers = groupRepository.getUsersByGroupId(groupRepository.getGroupId(user.getId()));
+        groupMembers.remove(user.getId());
+        List<User> myGroupMembers = new ArrayList<>();
+        for(int i = 0; i<groupMembers.size(); i++){
+            Optional<User> optionalUser =  userRepository.findById(groupMembers.get(i));
+            optionalUser.ifPresent(myGroupMembers::add);
+        }
+        return ResponseEntity.ok(matchService.getAllUserInfoAndPreferences(myGroupMembers));
+    }
 }
