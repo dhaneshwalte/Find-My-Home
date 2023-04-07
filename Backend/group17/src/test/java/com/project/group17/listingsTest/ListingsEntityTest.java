50aa8bd597a79966166020ae05593f21043f1d75
Sreya Gajjarapu <sr228618@dal.ca>
2023-04-07 12:37:16 -0300

    Testing getter and setter methods of listing ID

---
+++ Backend/group17/src/test/java/com/project/group17/listingsTest/ListingsEntityTest.java
@@ -0,0 +1,29 @@
+package com.project.group17.listingsTest;
+import com.project.group17.user.entity.User;
+import com.project.group17.listings.entity.ListingsEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class ListingsEntityTest {
+    private ListingsEntity listingsEntity;
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = mock(User.class);
+        Mockito.when(user.getId()).thenReturn(1);
+        Mockito.when(user.getUsername()).thenReturn("testUser");
+        listingsEntity = new ListingsEntity();
+        listingsEntity.setListingId(1L);
+
+    }
+
+    @Test
+    public void testListingId() {
+        assertEquals(1L, listingsEntity.getListingId());
+    }
+
+}
