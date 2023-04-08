ff8138cf103e18730a657b8c7e5608c8fab7cc38
Sreya Gajjarapu <sr228618@dal.ca>
2023-04-08 12:29:32 -0300

    Test cases for all endpoints in ListingController class

--- Backend/group17/src/test/java/com/project/group17/listingsTest/ListingsControllerTest.java
+++ Backend/group17/src/test/java/com/project/group17/listingsTest/ListingsControllerTest.java
@@ -1,66 +1,119 @@
 package com.project.group17.listingsTest;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.group17.LikeListing.entity.LikeListingEntity;
-import com.project.group17.LikeListing.repository.LikeListingRepository;
-import com.project.group17.listings.entity.ListingsEntity;
-import com.project.group17.listings.repository.ListingsRepository;
-import com.project.group17.listings.service.ListingsService;
+import com.project.group17.config.JwtService;
 import com.project.group17.listings.controller.ListingsController;
+import com.project.group17.listings.entity.ListingsEntity;
+import com.project.group17.listings.service.ListingsService;
 import com.project.group17.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(ListingsController.class)
-@Import(TestConfig.class)
+@WebMvcTest(controllers = ListingsController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ListingsControllerTest {
 
-@SpringBootTest
-public class ListingsControllerTest
-{
+    @Autowired
+    private MockMvc mockMvc;
 
-        @Autowired
-        private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        @Autowired
-        private ObjectMapper objectMapper;
+    @MockBean
+    private JwtService jwtService;
 
-        @MockBean
-        private ListingsService listingsService;
+    @MockBean
+    private ListingsService listingsService;
 
-        @MockBean
-        private LikeListingRepository likeListingRepository;
+    private List<ListingsEntity> listings;
+    private ListingsEntity listing1;
+    private ListingsEntity listing2;
 
-        @MockBean
-        private ListingsRepository listingsRepository;
+    @BeforeEach
+    void setUp() {
 
-        @Test
-        @WithMockUser(username = "testuser", roles = "USER")
-        public void testGetAllListings() throws Exception {
-            ListingsEntity listing1 = new ListingsEntity();
-            // Set listing1 properties
-            ListingsEntity listing2 = new ListingsEntity();
-            // Set listing2 properties
+        User user = new User();
+        user.setId(1);
+        user.setEmail("test@example.com");
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
-            when(listingsService.getAllListings()).thenReturn(Arrays.asList(listing1, listing2));
+        listing1 = new ListingsEntity();
+        listing1.setListingId(1L);
+        listing1.setType("1BHK Apartment");
 
-            mockMvc.perform(get("/api/v1/getAll")
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$[0].listingId").value(listing1.getListingId()))
-                    .andExpect(jsonPath("$[1].listingId").value(listing2.getListingId()));
-        }
+        listing2 = new ListingsEntity();
+        listing2.setListingId(2L);
+        listing2.setType("2BHK Apartment");
 
+        listings = Arrays.asList(listing1, listing2);
     }
 
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testAddListing() throws Exception {
+        String requestBody = "{\"listingId\":1,\"type\":\"Studio Apartment\",\"address\":null,\"utilities\":null,\"rent\":1234.0,\"details\":null,\"file64\":null,\"sfile64\":null}";
+
+        mockMvc.perform(post("/api/v1/listing")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().string("New listing is added"));
+    }
+    @Test
+    @WithMockUser
+    void testGetAllListings() throws Exception {
+        when(listingsService.getAllListings()).thenReturn(listings);
+
+        mockMvc.perform(get("/api/v1/getAll")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(listings)));
+    }
+
+    @Test
+    @WithMockUser
+    void testGetMyListings() throws Exception {
+        when(listingsService.getMyListings()).thenReturn(listings);
+
+        mockMvc.perform(get("/api/v1/get-my-listings")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(listings)));
+    }
+
+    @Test
+    @WithMockUser
+    void testGetLikedListings() throws Exception {
+        when(listingsService.getLikedListings()).thenReturn(listings);
+
+        mockMvc.perform(get("/api/v1/get-liked-listings")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(listings)));
+    }
+}
